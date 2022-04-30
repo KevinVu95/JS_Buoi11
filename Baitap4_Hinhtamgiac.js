ddaa1643485c3002144bafc3527f1d61cdacfbe1
NghiaVu <tcdtist>
2022-04-30 23:45:48 +0700

    firstsave

--- Baitap4_Hinhtamgiac.js
+++ Baitap4_Hinhtamgiac.js
@@ -1,12 +1,11 @@
-/*BÀI 4: Hàm này được gắn vào nút button
- Input: Tạo và lấy giá trị biến khi người dùng nhập 3 số
-Process(Pseudo code)
-1. Nếu a = b và b = c => tam giác đều
-2. Nếu a = b hoặc b = c hoặc c = a => tam giác cân
-3. Nếu a^2 + b^2 = c^2 hoặc b^2 = c^2 + a^2 hoặc a^2 = b^2 + c^2 => tam giác vuông
-4. Nếu k phải 3 loại trên, thì là loại khác
-Output: Loại tam giác
-*/
+// BÀI 4: Hàm này được gắn vào nút button
+//  Input: Tạo và lấy giá trị biến khi người dùng nhập 3 số
+// Process(Pseudo code)
+// 1. Nếu a = b và b = c => tam giác đều
+// 2. Nếu a = b hoặc b = c hoặc c = a => tam giác cân
+// 3. Nếu a^2 + b^2 = c^2 hoặc b^2 = c^2 + a^2 hoặc a^2 = b^2 + c^2 => tam giác vuông
+// 4. Nếu k phải 3 loại trên, thì là loại khác
+// Output: Loại tam giác
 
 function myFunction4(){
     var number1 = document.getElementById("canhA").value
