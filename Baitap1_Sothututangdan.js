1bba36bb685fcffcfc42ba9c816cd3e63867f8c6
NghiaVu <tcdtist>
2022-04-30 22:51:51 +0700

    secondsave

--- Baitap1_Sothututangdan.js
+++ Baitap1_Sothututangdan.js
@@ -1,13 +1,23 @@
-var valueNum1 = document.getElementById("num1").value;
-var valueNum2 = document.getElementById("num2").value;
-var valueNum3 = document.getElementById("num3").value;
-var min;
-var med;
-var max;
+// BÀI 1: Hàm này được gắn vào nút button
+//  Input: Tạo và lấy giá trị biến khi người dùng nhập 3 số nguyên
+// Process:
+// 1. Vì chỉ có 3 số nên giá trị tăng dần sẽ là var min, mid và max
+// 2. So sánh lần lượt để tìm ra số min và max
+// 3. Số mid = tổng 3 số - số min - số max
+// Output: Thứ tự từ trái qua phải là min, mid, max
 
-function myFunction() {
-    document.getElementById ("calcEl").innerHTML = ($ "min" $ "med" $ "max")
-} 
+function myFunction(){
+    var number1 = +document.getElementById("num1").value;
+    var number2 = +document.getElementById("num2").value;
+    var number3 = +document.getElementById("num3").value;
 
+    var min = number1 < number2 ? number1: number2;
+    var max = number1 > number2 ? number1 : number2;
+    min = min < number3 ? min : number3;
+    max = max > number3 ? max : number3;
+    var mid = number1 + number2 + number3 - min - max;
 
-console.log("calcEl");
+    document.getElementById("firstResult").style.display = 'block';
+    document.getElementById("order").innerHTML =` ${min} ${mid} ${max}`
+
+}
--- Baitap2_Vietloichao.js
+++ Baitap2_Vietloichao.js
@@ -1,19 +1,15 @@
-var B = "Bố";
-var M = "Mẹ";
-var A = "Anh Trai";
-var E = "Em Gái";
-var result;
+// BÀI 2: Hàm này được gắn vào nút button
+// Input: Tạo biến WhoIsThere và lấy giá trị khi người dùng chọn Bố hoặc Mẹ hoặc Anh trai hoặc Em gái
+// Process(Pseudo code)
+// 1. Cho hiện result2 khi nhấn vào button
+// 2. Chèn vào greeting dòng Xin chào + giá trị biến WhoIsThere
+// Output: Nhấn button sẽ hiện Xin chào + giá trị biến WhoIsThere
 
-function chooseEL = document.getElementById (choose){
-    
-    chooseEL.innerHTML = welcomeEl;
-    
- 
-    console.log(result) ;
-
-
-
-
+function myFunction2(){
+    var whoIsThere = document.getElementById("whoRU").value
+    document.getElementById("secondResult").style.display = 'block';
+    document.getElementById("helloThere").innerHTML = `"Xin chào ${whoIsThere}`
+}
 
 
 
